cfd2d66bde7ef850cda213f4c2a3ce021410c340
Thomas Upton <tom@shipwell.com>
2020-11-10 22:55:38 -0600

    Add theme and first slide

---
+++ theme.js
@@ -0,0 +1,20 @@
+import { future, highlight } from "@mdx-deck/themes";
+
+export default {
+  ...future,
+  ...highlight,
+  styles: {
+    a: {
+      color: "#006BA2",
+    },
+  },
+  colors: {
+    text: "#FFFFFF",
+    background: "#26282B",
+    primary: "#006BA2",
+  },
+  fonts: {
+    body: "Roboto",
+  },
+  googleFont: "https://fonts.googleapis.com/icon?family=Roboto:300,400,500,700",
+};
